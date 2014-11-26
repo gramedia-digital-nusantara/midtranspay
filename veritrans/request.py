bd7defb67056770d169381f26a478bfeea9ffe89
Derek J. Curtis <derek.curtis@apps-foundry.com>
2014-11-26 16:06:34 +0800

    Refactored request entity validation logic into a base class

--- veritrans/request.py
+++ veritrans/request.py
@@ -1,11 +1,31 @@
 import re
 
 
-class RequestBase(object):
-    pass
+class RequestEntityBase(object):
+
+    def validate_attr(self, name, value, pattern):
+        if not re.match(pattern, value, re.DOTALL):
+            raise ValueError(
+                "{attr_name} did not pass validation. ("
+                "Expression: {validation_expr} | "
+                "Value: {attr_value})".format(
+                    attr_name=name,
+                    validation_expr=pattern,
+                    attr_value=value))
+
+    def validate_all(self):
+        ''' Validates all of the current instance's properties.
+        If one of the instances does not validate, then a ValueError
+        will be thrown on the first failure.
+        '''
+        for attr_name in self._validators:
+            # in the event of None, we're switching to "" to validate
+            value = getattr(attr_name) or ''
+            validator = self._validators[attr_name]
+            self.validate_attr('attr_name', value, validator)
 
 
-class Address(object):
+class Address(RequestEntityBase):
 
     _validators = {'address': r'^(.{1,200})$',
                    'city': r'^(.{1,20})$',
@@ -29,24 +49,3 @@ class Address(object):
         self.country_code = country_code
 
         super(Address, self).__init__()
-
-    def validate_attr(self, name, value, pattern):
-        if not re.match(pattern, value, re.DOTALL):
-            raise ValueError(
-                "{attr_name} did not pass validation. ("
-                "Expression: {validation_expr} | "
-                "Value: {attr_value})".format(
-                    attr_name=name,
-                    validation_expr=pattern,
-                    attr_value=value))
-
-    def validate_all(self):
-        ''' Validates all of the current instance's properties.
-        If one of the instances does not validate, then a ValueError
-        will be thrown on the first failure.
-        '''
-        for attr_name in self._validators:
-            # in the event of None, we're switching to "" to validate
-            value = getattr(attr_name) or ''
-            validator = self._validators[attr_name]
-            self.validate_attr('attr_name', value, validator)
